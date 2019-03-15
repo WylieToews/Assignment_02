1e88ec0f60c1856a5f182c97aedb809ceb03fef5
WylieToews <47130424+WylieToews@users.noreply.github.com>
2019-03-14 20:57:31 -0600

    finalized videoFinal - added a random function for sound clips

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -16,36 +16,25 @@ var mainView = app.views.create('.view-main');
 document.addEventListener("deviceready", init, false);
 //init(); // use for testing on chrome
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function init() {
+
+    var soundFile = ["assets/button_drop.mp3", "assets/coins.mp3", "assets/elastic.mp3", "assets/soggy.mp3", "assets/swoosh.mp3", "assets/zipper.mp3"];
+    var x = Math.floor((Math.random() * 6));
+    console.log("Audio file = ", x);
+
+    $(".audioPlayer").html('<audio controls id="playSound"><source id="soundArray" src="' + soundFile[x] + '"></audio>');
+
+    var sound = document.getElementById("playSound");
+
+
     console.log("Ready to Roll - Camera")
 
     var vidOptions = {
         limit: 1,
         duration: 10,
         quality: 1
-    }  
-    
+    }
+
     $("#takeVid").on("click", vidCapture);
 
     function vidCapture() {
@@ -61,12 +50,25 @@ function init() {
 
         console.log(path)
 
-        $("#vidList").append('<video controls><source src="' + path + '" type="' + type + '" ></video>')
+        $("#vidList").append('<video controls id="videoFinal"><source  src="' + path + '" type="' + type + '" ></video>')
+
+        
+
+        var vid = document.getElementById("videoFinal");
+        vid.muted = true;
+        
+        vid.onplaying = function (){
+            sound.play();
+        };
     }
 
     function vidError(e) {
         console.log("Video did not work")
     }
 
-}
 
+
+
+
+
+}
